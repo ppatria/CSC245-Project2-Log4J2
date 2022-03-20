62bf2b95cb602fcee1fb14f56d6d0df115353fe1
Doug Lundin <doug.lundin@gmail.com>
2022-03-20 10:19:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Secure Software Development (CSC245) Log4J2 project
+
+Requires JDK 8 or more recent
+
+
+20220320 ddl: This Java project was forked and modified for Arapahoe Community College's CSC245 class. The MIT license allows for such usage.
+https://mkyong.com/logging/apache-log4j-2-tutorials/
+
---
+++ log4j2/src/main/java/edu/arapahoe/csc245/HelloWorld.java
@@ -0,0 +1,30 @@
+package edu.arapahoe.csc245;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloWorld {
+
+    private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("Hello from Log4j 2");
+
+        logger.debug("Example of a DEBUG {}", () -> 1);
+        logger.fatal("Example of a FATAL {}", () -> 2);
+        logger.error("Example of a ERROR {}", () -> 3);
+        logger.warn("Example of a WARN {}", () -> 4);
+        logger.info("Example of a INFO {}", () -> 5);
+        logger.trace("Example of a TRACE {}", () -> 6);
+
+//        while (true)//test rolling file
+//            logger.debug("hello {}", () -> getNumber());
+
+    }
+
+    static int getNumber() {
+        return 5;
+    }
+
+}
